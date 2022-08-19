484e11ca469a47fed75a2d1945327feefe8a4434
AJEET KUMAR <cajeet@hotmail.com>
2022-08-19 12:07:43 +0530

    Add files via upload

--- test-driven-range.js
+++ test-driven-range.js
@@ -26,26 +26,40 @@ function formRange(data) {
   let endsAt;
   let occurence = 1;
   let ranges = [];
-  sortedInput.forEach((element, index) => {
-    const difference = sortedInput[index + 1] - sortedInput[index];
-    if ([0, 1].includes(difference)) {
-      endsAt = index + 1;
-      occurence++;
-    } else {
-      if (occurence > 1) {
-        ranges.push({
-          min: sortedInput[startAt],
-          max: sortedInput[endsAt],
-          occurence,
-        });
-        endsAt = undefined;
-        occurence = 1;
-      }
-      startAt = index + 1;
-    }
-  });
 
-  printRange(ranges);
+  if (sortedInput.length > 1) {
+    sortedInput.forEach((element, index) => {
+      const difference = getDifference(
+        sortedInput[index + 1],
+        sortedInput[index]
+      );
+      if ([0, 1].includes(difference)) {
+        endsAt = index + 1;
+        occurence++;
+      } else {
+        if (occurence > 1) {
+          ranges.push({
+            min: sortedInput[startAt],
+            max: sortedInput[endsAt],
+            occurence,
+          });
+          endsAt = undefined;
+          occurence = 1;
+        }
+        startAt = index + 1;
+      }
+    });
+  } else {
+    ranges = [
+      {
+        min: data[0],
+        max: data[0],
+        occurence: 1,
+      },
+    ];
+  }
+
+  return ranges;
 }
 
 function printRange(ranges) {
@@ -55,10 +69,17 @@ function printRange(ranges) {
   });
 }
 
-formRange([3, 3, 4, 5, 7, 8, 9]);
+function getDifference(number1, number2) {
+  if (number1 !== undefined || number2 != undefined) {
+    return number1 - number2;
+  }
+}
+
+printRange(formRange([3]));
 
 module.exports = {
   isNotEmpty,
   isArray,
   formRange,
+  getDifference,
 };
--- test/test-driven-range.test.js
+++ test/test-driven-range.test.js
@@ -41,32 +41,67 @@ describe('charge range with test driven development', () => {
     expect(actual1).equals('Input is empty');
   });
 
+  it('formRange() should check for the one value', () => {
+    let input2 = [1];
+    let actual2 = charge.formRange(input2);
+    let expectedObject = [{ min: 1, max: 1, occurence: 1 }];
+    expect(actual2).deep.equal(expectedObject);
+  });
+
   it('formRange() should check for the two value', () => {
     let input2 = [1, 2];
     let actual2 = charge.formRange(input2);
-    let expectedObject = {
-      '1-2': 2,
-    };
+    let expectedObject = [
+      {
+        min: 1,
+        max: 2,
+        occurence: 2,
+      },
+    ];
     expect(actual2).deep.equal(expectedObject);
   });
 
   it('formRange() should check for range of repeated value', () => {
     let input3 = [1, 2, 5, 5, 6, 7];
     let actual3 = charge.formRange(input3);
-    let expectedObject1 = {
-      '1-2': 2,
-      '5-7': 4,
-    };
+    let expectedObject1 = [
+      {
+        min: 1,
+        max: 2,
+        occurence: 2,
+      },
+      {
+        min: 5,
+        max: 7,
+        occurence: 4,
+      },
+    ];
     expect(actual3).deep.equal(expectedObject1);
   });
 
   it('printRange() should Print Ranges', () => {
     let input3 = [1, 2, 5, 5, 6, 7];
     let actual3 = charge.printRange(input3);
-    let expectedObject1 = {
-      '1-2': 2,
-      '5-7': 4,
-    };
+    let expectedObject1 = [
+      {
+        min: 1,
+        max: 2,
+        occurence: 2,
+      },
+      {
+        min: 5,
+        max: 7,
+        occurence: 4,
+      },
+    ];
     expect(actual3).deep.equal(expectedObject1);
+  });
+
+  it('getDifference() should print the difference of two numbers', () => {
+    let number1 = 2,
+      number2 = 4;
+    let actual = charge.getDifference(number1, number2);
+    let expected = -2;
+    expect(actual).deep.equal(expected);
   });
 });
